e4cc4bff0ec34070b073e71c57b791bc6edc827e
saddam husain <saddamkhan.khan705@gmail.com>
2022-06-16 13:07:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <div class="container">
+  <div class="nav">nav</div>
+  <div class="slider">slider</div>
+  <div class="main">main</div>
+  <div class="content1">content1</div>
+  <div class="content2">content2</div>
+  <div class="content3">content3</div>
+  <div class="footer">footer</div>
+</div>
+    </div>
+  );
+}
+
+export default App;
